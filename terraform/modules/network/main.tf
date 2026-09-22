74994e856ce518a09dee3c08803c4a233d9d8e09
Rodrigo Serafim <148154291+rodrigo-srf@users.noreply.github.com>
2026-09-22 17:48:29 -0300

    style: format network Terraform module

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -2,6 +2,7 @@ resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name      = "${var.project_name}-vpc"
     ManagedBy = "Terraform"
@@ -10,6 +11,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
+
   tags = {
     Name      = "${var.project_name}-igw"
     ManagedBy = "Terraform"
@@ -18,6 +20,7 @@ resource "aws_internet_gateway" "this" {
 
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
+
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
@@ -46,6 +49,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public)
+
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
