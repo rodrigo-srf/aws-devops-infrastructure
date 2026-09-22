5268027129670361bc20b62112c544fb6c86cebe
Rodrigo Serafim <148154291+rodrigo-srf@users.noreply.github.com>
2026-09-22 17:45:52 -0300

    feat: define compute module inputs

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,22 @@
+variable "project_name" { type = string }
+variable "aws_region" { type = string }
+variable "vpc_id" { type = string }
+variable "subnet_id" { type = string }
+variable "ami_id" { type = string }
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "container_image" {
+  description = "ECR image URI including tag."
+  type        = string
+}
+
+variable "ecr_repository_arn" { type = string }
+
+variable "allowed_http_cidrs" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
